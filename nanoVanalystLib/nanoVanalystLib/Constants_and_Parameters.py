c95ffc9dfad75734acd5b59c6a2f1cb032e58da7
Ling Wang@dracula-K50IE <LingWangNeuralEng@gmail.com>
2015-03-12 13:47:43 -0700

    add constants

---
+++ nanoVanalystLib/nanoVanalystLib/Constants_and_Parameters.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on Mar 12, 2015-12:33:45 AM
+
+@author: Ling Wang<LingWangNeuralEng@gmail.com>
+'''
+from LWpyUtils.Constants_and_Parameters.LWPY_ENVS import *
+K_PorAnalyzer_LogFile = 'PorAnalyzer_LogFile'
+K_PoreAnalyzer_IMshowWait = "PoreAnalyzer_IMshowWait"
+K_PoreAnalyzer_IMprefix = "PoreAnalyzer_IMprefix"
+K_FLAG_SHOW_IMG = "FLAG_SHOW_IMG"
